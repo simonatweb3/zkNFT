7edfdf5bb018d88ffe3ff064542591f089778a2b
samzkback <geeksam820@gmail.com>
2023-07-05 15:19:03 +0800

    generate trapdoor/nullifier from signature

--- packages/jssdk/src/Sdk.ts
+++ packages/jssdk/src/Sdk.ts
@@ -4,18 +4,11 @@ export const POMP_KEY_SIGN_MESSAGE =
   "Sign this message to generate your Pomp Privacy Key. This key lets the application decrypt your identity on Pomp.\n\nIMPORTANT: Only sign this message if you trust the application.";
 
 export class PompSdk {
-  public async getAccountKeySigningData(signer : Signer) {
+  public async generateAccountPrivKeys(signer : Signer) {
     console.log('Balance:', ethers.utils.formatEther(650000000));
-    //return signer.signMessage(Buffer.from(POMP_KEY_SIGN_MESSAGE))
-    return signer.signMessage(POMP_KEY_SIGN_MESSAGE)
+    const signature = await signer.signMessage(POMP_KEY_SIGN_MESSAGE)
+    const trapdoor = ethers.utils.hexlify('0x' + signature.slice(2, 34))
+    const nullifier = ethers.utils.hexlify('0x' + signature.slice(34, 66))
+    return { trapdoor, nullifier };
   }
-
-  // public async generateAccountKeyPair(account: EthAddress, provider = this.provider) {
-  //   const ethSigner = new Web3Signer(provider);
-  //   const signingData = this.getAccountKeySigningData();
-  //   const signature = await ethSigner.signPersonalMessage(signingData, account);
-  //   const privateKey = signature.slice(0, 32);
-  //   const publicKey = await this.derivePublicKey(privateKey);
-  //   return { publicKey, privateKey };
-  // }
 }
--- packages/jssdk/test/sdk.test.ts
+++ packages/jssdk/test/sdk.test.ts
@@ -1,17 +1,17 @@
 //import {PompSdk} from "@pomp-eth/jssdk/src/Sdk"
-import { PompSdk, POMP_KEY_SIGN_MESSAGE } from "../src/sdk.js"
+//import { expect } from "chai";
+import { PompSdk } from "../src/sdk.js"
 import { ethers, Signer, Wallet } from "ethers";
-import { expect } from "chai";
 
 async function test() {
   const sdk = new PompSdk();
 
     const PRIV_KEY = "0x828a065aa2818619cb9a5435ce9e7d95fdd3e6dd89fc5fcd4dd4a37346a54084" // 0x7A7765Db4733DFe037342A8bCDfAEE83ddE405da
     let signer : Signer = new Wallet(PRIV_KEY, ethers.getDefaultProvider())
-    let signature = await sdk.getAccountKeySigningData(signer)
-    console.log("user signature : ", signature);
+    let keys = await sdk.generateAccountPrivKeys(signer)
+    console.log("keys : ", keys);
 
-	  expect(ethers.utils.verifyMessage(POMP_KEY_SIGN_MESSAGE, signature)).equal(await signer.getAddress())
+	  //expect(ethers.utils.verifyMessage(POMP_KEY_SIGN_MESSAGE, signature)).equal(await signer.getAddress())
 }
 
 //abc
