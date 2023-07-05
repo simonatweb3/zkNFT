220af80f384d9d401426c517447a29c9587a188c
samzkback <geeksam820@gmail.com>
2023-07-05 18:18:33 +0800

    involve @semaphore-protocol/identity for generate identity, have to disable noImplicitAny
    as there is an error when import @semaphore-protocol/identity:
    
    src/sdk.ts:2:26 - error TS7016: Could not find a declaration file for module '@semaphore-protocol/identity'. '/Users/sam/pomp-eth/node_modules/@semaphore-protocol/identity/dist/index.mjs' implicitly has an 'any' type.
      Try `npm i --save-dev @types/semaphore-protocol__identity` if it exists or add a new declaration (.d.ts) file containing `declare module '@semaphore-protocol/identity';`
    
    2 import { Identity } from "@semaphore-protocol/identity"

--- packages/jssdk/src/Sdk.ts
+++ packages/jssdk/src/Sdk.ts
@@ -1,14 +1,21 @@
 import {ethers, Signer} from "ethers"
+import { Identity } from "@semaphore-protocol/identity"
 
 export const POMP_KEY_SIGN_MESSAGE =
   "Sign this message to generate your Pomp Privacy Key. This key lets the application decrypt your identity on Pomp.\n\nIMPORTANT: Only sign this message if you trust the application.";
 
 export class PompSdk {
   public async generateAccountPrivKeys(signer : Signer) {
-    console.log('Balance:', ethers.utils.formatEther(650000000));
     const signature = await signer.signMessage(POMP_KEY_SIGN_MESSAGE)
     const trapdoor = ethers.utils.hexlify('0x' + signature.slice(2, 34))
     const nullifier = ethers.utils.hexlify('0x' + signature.slice(34, 66))
     return { trapdoor, nullifier };
   }
+
+  public async generateIdentity() {
+    const identity = new Identity("identity")
+    const identityCommitment = identity.getCommitment()
+    return identityCommitment
+  }
+
 }
