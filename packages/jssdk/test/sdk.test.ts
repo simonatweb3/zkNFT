fbf9458931c510d6147d2fd557b0d5f63488db2a
samzkback <geeksam820@gmail.com>
2023-07-05 10:49:47 +0800

    [TO-BE-FIX]test framework : describe/it
    yarn mocha --require ts-node/register test/sdk.test.ts
    
    TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts" for /Users/sam/pomp-eth/packages/jssdk/test/sdk.test.ts

--- packages/jssdk/test/sdk.test.ts
+++ packages/jssdk/test/sdk.test.ts
@@ -3,17 +3,20 @@ import { PompSdk, POMP_KEY_SIGN_MESSAGE } from "../src/sdk.js"
 import { ethers, Signer, Wallet } from "ethers";
 import { expect } from "chai";
 
-async function test() {
-  const sdk = new PompSdk();
+describe("POMP-ETH SDK Unit Test", function () {
 
+  let signer: Signer
+  let sdk : PompSdk
+  before(async () => {
+    sdk = new PompSdk();
     const PRIV_KEY = "0x828a065aa2818619cb9a5435ce9e7d95fdd3e6dd89fc5fcd4dd4a37346a54084" // 0x7A7765Db4733DFe037342A8bCDfAEE83ddE405da
-    let signer : Signer = new Wallet(PRIV_KEY, ethers.getDefaultProvider())
+    signer = new Wallet(PRIV_KEY, ethers.getDefaultProvider())
+  });
+
+  it("Account Derive", async () => {
     let signature = await sdk.getAccountKeySigningData(signer)
     console.log("user signature : ", signature);
-
 	  expect(ethers.utils.verifyMessage(POMP_KEY_SIGN_MESSAGE, signature)).equal(await signer.getAddress())
-}
+  });
 
-//abc
-
-test();
+});
