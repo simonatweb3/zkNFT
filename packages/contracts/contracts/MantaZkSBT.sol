5c1b789872739ac1ef14cbd007cfbb3fd8e47783
“senn” <sennhanami@163.com>
2023-07-12 17:59:47 +0800

    add SBT logic

---
+++ packages/contracts/contracts/MantaZkSBT.sol
@@ -0,0 +1,148 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Events.sol";
+
+contract ZkSBT is ERC721URIStorage, Ownable, Events {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // single reserve for one address
+    uint256 public mintsPerReserve;
+
+    // operators allowed to mint SBT
+    mapping(address => bool) public operators;
+
+    // stores allowed assetId:  [start id , end id]
+    mapping(address => uint256[2]) public allowedAssetIds;
+
+    // sbt tokenId => sbt's metaData
+    mapping(uint256 => MetaData) public sbtMetaData;
+
+    // mintId status
+    mapping(uint256 => bool) public mintIdStatus;
+
+    // mintId => baseUrl
+    mapping(uint256 => string) public sbtBatchBaseUrl;
+
+    // address of pomp, stores corresponding Merkle tree;
+    address public pomp;
+
+    modifier onlyOperator() {
+        require(operators[msg.sender] || msg.sender == owner());
+        _;
+    }
+
+    // asset amount range
+    enum RANGE {
+        RANGE_0, // >0
+        RANGE_1_10, // 1~10
+        RANGE_10_100, // 10~100
+        RANGE_100 // >100
+    }
+
+    // metadata of SBT
+    struct MetaData {
+        // used to distinguish different mint batch
+        uint256 mintId;
+        uint256 chainId;
+        address assetContractAddress;
+        uint256 assetTokenId;
+        RANGE range;
+    }
+
+    constructor(address _pomp) ERC721("ZkSBT", "ZkSBT") {
+        pomp = _pomp;
+    }
+
+    function safeMint(address to) public onlyOperator {
+        uint256 tokenId = _tokenIds.current();
+        _tokenIds.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function mintWithReservedId(
+        uint256 identityCommitment, //record identity commitment of user
+        uint256 mintId, //mint id
+        uint256 chainId, // chainId of the asset
+        address assetContractAddress, //asset contract address
+        uint256 assetTokenId, //asset token id
+        uint256 sbtId, //sbt id in the per sbt identity
+        RANGE range
+    ) public onlyOperator {
+        require(identityCommitment != address(0), "invalid identityCommitment");
+        require(mintIdStatus[mintId], "mintId not available");
+
+        (uint256 _startId, uint256 _endId) = allowedAssetIds[
+            identityCommitment
+        ];
+
+        require(
+            _startId != 0 && _endId != 0 && _startId < _endId,
+            "assetId not reserved"
+        );
+
+        uint256 tokenId = _startId;
+        require(sbtId == tokenId, "Invalid sbtId");
+        _safeMint(identityCommitment, tokenId);
+
+        sbtMetaData[tokenId] = MetaData(
+            mintId,
+            chainId,
+            assetContractAddress,
+            assetTokenId,
+            range
+        );
+    }
+
+    /**
+     * @dev Approve `operator` to mint zkSBT
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setOperator(
+        address operator,
+        bool approved
+    ) internal virtual onlyOwner {
+        operators[operator] = approved;
+        emit OperatorChange(operator, approved);
+    }
+
+    /**
+     * @dev set status of mintId
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setOperator(
+        uint256 _mintId,
+        bool _open
+    ) internal virtual onlyOperator {
+        mintIdStatus[_mintId] = _open;
+        emit MintIdStatusChange(_mintId, _open);
+    }
+
+    /**
+     * @dev reserve SBT ids for user
+     *
+     * Emits an {ReserveSBT} event.
+     */
+    function reserve_sbt(address _to) internal virtual onlyOperator {
+        require(mintsPerReserve > 0, "invalid mintsPerReserve");
+
+        uint256 _startId = _tokenIds.current();
+
+        for (uint256 i = 0; i < mintsPerReserve; i++) {
+            _tokenIds.increment();
+        }
+
+        uint256 _endId = _startId + mintsPerReserve;
+
+        allowedAssetIds[_to] = [_startId, _endId];
+
+        emit ReserveSBT(_to, _startId, _endId);
+    }
+}
--- packages/contracts/contracts/pomp.sol
+++ packages/contracts/contracts/pomp.sol
@@ -5,50 +5,45 @@ import "@semaphore-protocol/contracts/base/SemaphoreGroups.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Pomp is SemaphoreGroups, Ownable {
-  enum ASSET {
-    ETH,
-    BNB
-  }
+    enum ASSET {
+        ETH,
+        BNB
+    }
 
-  enum RANGE {
-      RANGE_0,       // >0
-      RANGE_1_10,    // 1~10
-      RANGE_10_100,  // 10~100
-      RANGE_100      // >100
-  }
+    enum RANGE {
+        RANGE_0, // >0
+        RANGE_1_10, // 1~10
+        RANGE_10_100, // 10~100
+        RANGE_100 // >100
+    }
 
-  // asset_type --> asset_range --> merkle tree)
-  mapping(uint => mapping(uint => uint)) groups;
+    // asset_type --> asset_range --> merkle tree)
+    mapping(uint => mapping(uint => uint)) groups;
 
-  // external nullifier, increase per verify
-  mapping(uint => mapping(uint => uint)) salts;  // random?
-  mapping(uint => bool) nullifierHashes;
+    // external nullifier, increase per verify
+    mapping(uint => mapping(uint => uint)) salts; // random?
+    mapping(uint => bool) nullifierHashes;
 
-  constructor() Ownable() {
-    // init build-in pomp pool
-    // createPompPool
-  }
+    constructor() Ownable() {
+        // init build-in pomp pool
+        // createPompPool
+    }
 
-  function createPompPool(
-    uint asset,
-    uint range
-  ) public onlyOwner {
-    // createGroup
-  }
+    function createPompPool(uint asset, uint range) public onlyOwner {
+        // createGroup
+    }
 
-  // batch mint
-  function mint(
-    uint[] calldata identity,
-    uint asset,
-    uint range
-  ) public onlyOwner {
-    // addMember
+    // batch mint
+    function mint(
+        uint[] calldata identity,
+        uint asset,
+        uint range
+    ) public onlyOwner {
+        // addMember
+        // sbt[id] = identity
+    }
 
-    // sbt[id] = identity
-  }
-
-  function verify() public {
-    // PompVerifier.verifyProof()
-  }
-
+    function verify() public {
+        // PompVerifier.verifyProof()
+    }
 }
