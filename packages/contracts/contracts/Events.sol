9211779385418d1f98920175c551e4356a05de0b
senn <sennhanami@163.com>
2023-07-14 17:14:21 +0800

    delete separate event file

--- packages/contracts/contracts/Events.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.4;
-
-contract Events {
-    event OperatorChange(address indexed _operator, bool indexed _allowed);
-
-    event MintIdStatusChange(uint256 indexed _mintId, bool indexed _open);
-}
