69ea1148bd0e217b7455fb1a465fe826b3ddb345
senn <sennhanami@163.com>
2023-07-14 17:15:15 +0800

    comment out the code for test

--- packages/contracts/contracts/verifier.sol
+++ packages/contracts/contracts/verifier.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@pomp-eth/circuits/contracts/pomp_verifier.sol";
+// import "@pomp-eth/circuits/contracts/pomp_verifier.sol";
