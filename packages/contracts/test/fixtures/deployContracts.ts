3f1f7b6796304a61f39a12978d0d9a88c6e96436
senn <sennhanami@163.com>
2023-07-15 10:14:25 +0800

    upgrade ethers to 6.6.3 to fit hardhat ether type

--- packages/contracts/test/fixtures/deployContracts.ts
+++ packages/contracts/test/fixtures/deployContracts.ts
@@ -1,17 +1,12 @@
 import { ethers } from "hardhat";
-import { Wallet } from "ethers";
+
 import { randomHex } from "../../utils/encoding";
 
 import { keccak256 } from "ethers/lib.commonjs/crypto";
 import { toUtf8Bytes } from "ethers/lib.commonjs/utils";
 
 import { faucet } from "../../utils/faucet";
-import {
-  ZkSBT__factory,
-  ZkSBT,
-  Pomp__factory,
-  Pomp,
-} from "../../typechain-types";
+import { ZkSBT, Pomp } from "../../typechain-types";
 
 export async function deployContracts() {
   let zkSBT: ZkSBT;
@@ -19,19 +14,16 @@ export async function deployContracts() {
 
   const { provider } = ethers;
 
-  const ownerOfPompContract: Wallet = new ethers.Wallet(
+  const ownerOfPompContract = new ethers.Wallet(randomHex(32), provider as any);
+  const ownerOfZkSbtContract = new ethers.Wallet(
     randomHex(32),
     provider as any
   );
-  const ownerOfZkSbtContract: Wallet = new ethers.Wallet(
+  const operatorOfZkSbtContract = new ethers.Wallet(
     randomHex(32),
     provider as any
   );
-  const operatorOfZkSbtContract: Wallet = new ethers.Wallet(
-    randomHex(32),
-    provider as any
-  );
-  const user_1: Wallet = new ethers.Wallet(randomHex(32), provider as any);
+  const user_1 = new ethers.Wallet(randomHex(32), provider as any);
 
   await faucet(ownerOfPompContract.address, provider as any);
   await faucet(ownerOfZkSbtContract.address, provider as any);
